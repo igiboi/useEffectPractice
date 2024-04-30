06bf166d2d2c0e0fa66f369cee45bb0acace9c78
Luigi Matias <luigi.matias@codehunter.com>
2024-04-30 11:44:52 -0400

    added sideEffect to find geolocation to referr the closest location using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 import Places from './components/Places.jsx';
 import { AVAILABLE_PLACES } from './data.js';
 import Modal from './components/Modal.jsx';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
 import logoImg from './assets/logo.png';
+import { sortPlacesByDistance } from './loc.js';
 
 function App() {
   const modal = useRef();
   const selectedPlace = useRef();
+  const [availablePlaces, setAvailablePlaces] = useState(AVAILABLE_PLACES);
   const [pickedPlaces, setPickedPlaces] = useState([]);
+
+  useEffect(() => {
+    // This code is a sideEffect
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+
+      setAvailablePlaces(sortedPlaces);
+    });
+  }, []);
 
   function handleStartRemovePlace(id) {
     modal.current.open();
@@ -47,7 +62,7 @@ function App() {
       </Modal>
 
       <header>
-        <img src={logoImg} alt="Stylized globe" />
+        <img src={logoImg} alt='Stylized globe' />
         <h1>PlacePicker</h1>
         <p>
           Create your personal collection of places you would like to visit or
@@ -62,8 +77,9 @@ function App() {
           onSelectPlace={handleStartRemovePlace}
         />
         <Places
-          title="Available Places"
-          places={AVAILABLE_PLACES}
+          title='Available Places'
+          places={availablePlaces}
+          fallbackText={'Sorting places by distance...'}
           onSelectPlace={handleSelectPlace}
         />
       </main>
